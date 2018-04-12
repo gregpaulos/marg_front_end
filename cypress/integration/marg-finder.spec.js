698d58194fbe6cfe95cb4906ce55b8d544e8060b
Michael Schreier <mjschreier1@gmail.com>
2018-04-12 11:53:57 -0600

    completed second test

--- cypress/integration/marg-finder.spec.js
+++ cypress/integration/marg-finder.spec.js
@@ -1,7 +1,20 @@
 describe("Marg-Finder Application", () => {
+
 	it("has a header component rendered with the h1 of the app title", () => {
 		cy.visit("/")
 		cy.get("header h1")
 			.should("contain", "Margarita-Finder")
 	})
+
+	it("renders a <ul> with at least 5 <li> when #findDrinks is clicked", () => {
+		cy.get("#findDrinks")
+			.click()
+
+		cy.get("ul")
+			.find("li")
+			.should("have.length.at.least", 5)
+	})
+
+	it("")
+
 })
