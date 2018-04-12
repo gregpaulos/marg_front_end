7b97fc55fa327a2bca0b4b07f4b116a8c7901559
Michael Schreier <mjschreier1@gmail.com>
2018-04-12 13:54:47 -0600

    completed tests for first three user story acceptance criteria

--- cypress/integration/marg-finder.spec.js
+++ cypress/integration/marg-finder.spec.js
@@ -15,6 +15,60 @@ describe("Marg-Finder Application", () => {
 			.should("have.length.at.least", 5)
 	})
 
-	// it("")
+	it("displays an error message prompting the user to enable geo-location if #findDrinks is clicked with geo-location disabled", () => {
+		cy.request("GET", "http://localhost:3000/margs/undefined/undefined")
+			.then(response => {
+        expect(response.body).to.have.property("message", "Please enable geo-location and try again")
+      })
+  })
+
+  it("renders a minimum of 5 margs sorted by proximity when #findDrinks is clicked", () => {
+    cy.get("#findDrinks")
+      .click()
+
+    cy.get("ul")
+      .find("li")
+      .should("have.length.at.least", 5)
+
+    cy.get("ul li span").eq(0)
+      .then($num0 => {
+        const num0 = $num.text()
+
+        cy.get("ul li span").eq(0)
+          .should($num1 => {
+            expect($num1.text()).to.be.greaterThan(num0)
+          })
+      })
+
+    cy.get("ul li span").eq(1)
+      .then($num1 => {
+        const num1 = $num.text()
+
+        cy.get("ul li span").eq(2)
+          .should($num2 => {
+            expect($num2.text()).to.be.greaterThan(num1)
+          })
+      })
+
+    cy.get("ul li span").eq(2)
+      .then($num2 => {
+        const num2 = $num.text()
+
+        cy.get("ul li span").eq(3)
+          .should($num3 => {
+            expect($num3.text()).to.be.greaterThan(num2)
+          })
+      })
+
+    cy.get("ul li span").eq(3)
+      .then($num3 => {
+        const num3 = $num.text()
+
+        cy.get("ul li span").eq(4)
+          .should($num4 => {
+            expect($num4.text()).to.be.greaterThan(num3)
+          })
+      })
+  })
 
 })
