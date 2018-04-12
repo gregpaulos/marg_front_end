cfdc9f6fc73a2806fe0e0a41583e561d8e5bb348
Zackary Moore <bowchkabowwow@gmail.com>
2018-04-12 13:59:20 -0600

    passing first test, created header and button

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button/Button';
+import ListArea from './ListArea/ListArea';
+
+const Main = () => {
+    return (
+        <main>
+            <Button />
+            <ListArea />
+        </main>
+    )
+}
+
+export default Main;
