d37c39dc70ea549feb36a54f86247a8a79b75398
Michael Schreier <mjschreier1@gmail.com>
2018-04-16 10:34:13 -0600

    added 5 markers to map

--- src/components/Main/MargMap/MargMap.js
+++ src/components/Main/MargMap/MargMap.js
@@ -15,13 +15,6 @@ class MargMap extends Component {
         });
     }
 
-    // markers = this.props.data.map(location => {
-    //     return <Marker 
-    //             lat={Number(location.lat)}
-    //             lng={Number(location.long)}
-    //             draggable={false} />
-    // });
-
     render() {
         return (
            <div> 
@@ -33,15 +26,19 @@ class MargMap extends Component {
             lng={this.props.location.lng}
             zoom={12}
             onMapCreated={this.onMapCreated} >
-            {this.markers}
             <InfoWindow 
-              lat={this.props.location.lat}
-              lng={this.props.location.lng}
-              content={'You are here!'} />
-              <Marker 
-              lat={Number(this.props.data[0].lat)}
-              lng={Number(this.props.data[0].long)} />
-              </Gmaps> }
+                lat={this.props.location.lat}
+                lng={this.props.location.lng}
+                content={'You are here!'} />
+            {this.props.data.map((location, i) => {
+                return (
+                    <Marker
+                        key={`marker-${i}`}
+                        lat={Number(location.lat)}
+                        lng={Number(location.long)} />
+                )
+            })}
+            </Gmaps> }
             </div>
         );
     }
