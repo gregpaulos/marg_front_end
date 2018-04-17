024c76c9de3575a7dc85810cb6db0c83157fa8bb
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-04-17 11:51:02 -0600

    when no ratings, dont render stars

--- src/components/Main/ListArea/ListArea.js
+++ src/components/Main/ListArea/ListArea.js
@@ -3,11 +3,21 @@ import ReactStars from "react-stars";
 
 const ListArea = props => {
   const list = props.establishments.map((establishment, i) => {
+    const starRating = establishment.avgRating ? (
+      <ReactStars
+        count={5}
+        size={24}
+        color2={"#ffd700"}
+        value={establishment.avgRating}
+        edit={false}
+      />
+    ) : (
+      "No Ratings Yet!"
+    );
+
     return (
       <li key={establishment.id} onClick={() => props.toggleModal(i)}>
-        {establishment.name}, {establishment.distance}, AVG RATING:
-        <ReactStars count={5} size={24} color2={'#ffd700'} value={establishment.avgRating} edit={false}
-        />
+        {establishment.name}, {establishment.distance}, AVG RATING: {starRating}
       </li>
     );
   });
