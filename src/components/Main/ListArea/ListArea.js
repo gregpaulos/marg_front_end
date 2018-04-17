297940bda055c09c6fc4f879eb929195db7da2c4
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-04-17 11:46:38 -0600

    got stars to render for ratings

--- src/components/Main/ListArea/ListArea.js
+++ src/components/Main/ListArea/ListArea.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
+import ReactStars from "react-stars";
 
-const ListArea = (props) => {
-    
-    const list = props.establishments.map((establishment, i)=>{
-        return (
-            <li key={establishment.id} onClick={() => props.toggleModal(i)}>{establishment.name}, {establishment.distance}, {establishment.avgRating} </li>
-        )
-    })
-
+const ListArea = props => {
+  const list = props.establishments.map((establishment, i) => {
     return (
-        <div>
-            <ul>
-                {list}
-            </ul>
-        </div>
-    )
+      <li key={establishment.id} onClick={() => props.toggleModal(i)}>
+        {establishment.name}, {establishment.distance}, AVG RATING:
+        <ReactStars count={5} size={24} color2={'#ffd700'} value={establishment.avgRating} edit={false}
+        />
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <ul>{list}</ul>
+    </div>
+  );
 };
 
 export default ListArea;
